44fb770373d7bcba9998f27cf6ae2164b5735b11
Richard Hsu <shengwh1@uci.edu>
2020-03-07 13:40:54 -0800

    Initial Commit

---
+++ SugoiFridge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SugoiFridge
+//
+//  Created by Richard Hsu on 2020/3/7.
+//  Copyright © 2020 TAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
